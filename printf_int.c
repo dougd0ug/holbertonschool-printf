c84301e529414567756e24e6794dcfa45951b4d7
LouisManchon <manchon.louis31@gmail.com>
2025-03-26 15:11:20 +0100

    task 1 du projet printf

---
+++ printf_int.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+int print_int(va_list args)
+{
+    int num = va_arg(args, int);
+    int count = 0;
+    char buffer[12];
+    int i = 0;
+
+    if (num == 0)
+    {
+        _putchar('0');
+        return 1;
+    }
+
+    if (num < 0)
+    {
+        _putchar('-');
+        count++;
+	num = -num
+    }
+
+    while (num > 0)
+    {
+        buffer[i++] = (num % 10) + '0';
+        num /= 10;
+    }
+
+    while (i > 0)
+    {
+        _putchar(buffer[--i]);
+        count++;
+    }
+
+    return count;
+}
